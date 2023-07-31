64f98f600e9b0bdef2219588aa8031423451d3cb
Swapnil1311 <thakore.swapnil@gmail.com>
2023-07-31 22:09:17 +0530

    Added express

--- server.js
+++ server.js
@@ -1,16 +1,52 @@
-// server creation
+const express = require("express");
 
-const http = require("http");
+// initilisation
+const app = express();
+
+// appln will now use json format for the data transfer
+app.use(express.json());
 
 const port = 8081;
 
-http.createServer((req,res) =>{
-    res.writeHead(200,{"Content-Type":"text/html"});
-    res.write("<h4> Hello, this is my new server</h4>");
-    res.end();
-})
-.listen(port,()=>{
-    console.log(`My Node.js server started on port ${port}`);
+const toDoList = ["Need to learn", "Need to code"];
+
+
+// http://localhost:8081/todos
+app.get("/todos", (req, res) => {
+  res.status(200).send(toDoList);
 });
 
-// http://localhost:8081
+app.post("/todos", (req, res) => {
+  let newToDoItem = req.body.item;
+  toDoList.push(newToDoItem);
+  res.status(201).send({
+    message: "The to do got added succesfully",
+  });
+});
+
+app.delete("/todos", (req, res) => {
+  const itemToDelete = req.body.item;
+
+  toDoList.find((element, index) => {
+    if (element === itemToDelete) {
+      toDoList.splice(index, 1);
+    }
+  });
+  res.status(202).send({
+    message: `Deleted item - ${req.body.item}`,
+  });
+});
+
+app.all("/todos", (req, res) => {
+  res.status(501).send();
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send();
+});
+
+// app.get("/todos/create");
+// app.get("/todos/deletd");
+app.listen(port, () => {
+  console.log(`Node js server started on ${port}`);
+});
